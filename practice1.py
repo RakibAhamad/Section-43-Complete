78a09b961176e3d46901d739abe25e4aa815acc0
Rakib Ahamad <rkbhasan123@gmail.com>
2026-01-01 12:57:51 +0600

    Complete Practice 1

--- practice1.py
+++ practice1.py
@@ -1,17 +1,15 @@
-from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QLabel, QWidget, QDialog, QComboBox,
-                             QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem)
+from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QDialog, QComboBox,
+                             QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem)
 import sys
 from PyQt6.QtGui import QAction
 import sqlite3
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
-
         file_menu_item = self.menuBar().addMenu("&File")
-        help_menu_item = self.menuBar().addMenu("&Help")
+        help_menu_item = self.menuBar().addMenu("& Help")
 
         add_student_action = QAction("Add Student", self)
         add_student_action.triggered.connect(self.insert)
@@ -23,10 +21,9 @@ class MainWindow(QMainWindow):
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.table.setHorizontalHeaderLabels(["ID", "Name", "Course", "Mobile"])
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
-
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
@@ -35,7 +32,6 @@ class MainWindow(QMainWindow):
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
             for column_number, data in enumerate(row_data):
-                print(row_data)
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
 
@@ -49,7 +45,6 @@ class InsertDialog(QDialog):
         self.setWindowTitle("Insert Student Data")
         self.setFixedWidth(300)
         self.setFixedHeight(300)
-
         layout = QVBoxLayout()
 
         # Add student name widget
@@ -57,22 +52,20 @@ class InsertDialog(QDialog):
         self.student_name.setPlaceholderText("Name")
         layout.addWidget(self.student_name)
 
-        # Add combo box of courses
+        # Add combo box of course
         self.course_name = QComboBox()
         courses = ["Biology", "Math", "Astronomy", "Physics"]
         self.course_name.addItems(courses)
         layout.addWidget(self.course_name)
 
-        # Add mobile widget
         self.mobile = QLineEdit()
         self.mobile.setPlaceholderText("Mobile")
         layout.addWidget(self.mobile)
 
-        # Add a submit button
+        # Add a mobile button
         button = QPushButton("Register")
         button.clicked.connect(self.add_student)
         layout.addWidget(button)
-
         self.setLayout(layout)
 
     def add_student(self):
@@ -81,14 +74,10 @@ class InsertDialog(QDialog):
         mobile = self.mobile.text()
         connection = sqlite3.connect("database.db")
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
-                       (name, course, mobile))
+        cursor.execute("INSERT INTO students(name, course, mobile) VALUES (?, ?, ?)", (name, course, mobile))
         connection.commit()
         cursor.close()
         connection.close()
-
-        main_window.load_data()
-
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
